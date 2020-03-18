a22ddcfcde3e62021bf254f831cac96c1f2fc083
RockpunkZikaGG <william.rocha.silva.wrs@gmail.com>
2020-03-18 02:23:58 -0300

    inicio

---
+++ site_de_eventos/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './header';
+import MainView from './main_view';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <MainView/>
+      
+    </div>
+  );
+}
+
+export default App;
